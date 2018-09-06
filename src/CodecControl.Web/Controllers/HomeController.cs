ad892666a304d9a1708363fd779aed9995dfea3c
Anders Stenberg <anders.stenberg@activesolution.se>
2018-09-06 17:27:09 +0200

    Defaultsida

---
+++ src/CodecControl.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CodecControl.Web.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodecControl.Web.Controllers
+{
+    [Route("")]
+    public class HomeController : ApiControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return $"Codec Control. {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+        }
+    }
+}

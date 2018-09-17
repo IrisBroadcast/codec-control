3ef17cb37daf0f81a51a825a9266482650607edb
Andreas Johansson <andreas.johansson@sr.se>
2018-09-17 15:38:10 +0200

    Lagt till CcmDbContext så att information om kodare kan läsas direkt från db

---
+++ src/CodecControl.Data/Database/CcmDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CodecControl.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodecControl.Data.Database
+{
+    public class CcmDbContext : DbContext
+    {
+        public CcmDbContext(DbContextOptions<CcmDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<RegisteredSipEntity> RegisteredSips { get; set; }
+    }
+}
---
+++ src/CodecControl.Data/Entities/UserAgentEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodecControl.Data.Entities
+{
+    // Kodarmodell
+    [Table("UserAgents")]
+    public class UserAgentEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Api { get; set; }
+        public string GpoNames { get; set; }
+        public int Inputs { get; set; }
+    }
+}

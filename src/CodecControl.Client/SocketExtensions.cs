bc54507b113164c0dbd1d6cd278a6947c0d5f643
Andreas Johansson <andreas.johansson@sr.se>
2018-09-21 16:27:14 +0200

    Lagt till extensionmetod för connectasync på socket (med timeout)

---
+++ src/CodecControl.Client/SocketExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodecControl.Client
+{
+    public static class SocketTaskExtensions
+    {
+        private const int ConnectionTimedOutStatusCode = 10060;
+
+        /// <summary>
+        /// Connects the specified socket.
+        /// </summary>
+        /// <param name="socket">The socket.</param>
+        /// <param name="endpoint">The IP endpoint.</param>
+        /// <param name="timeout">The timeout in milliseconds.</param>
+        public static async Task ConnectAsync(this Socket socket, EndPoint endpoint, int timeout)
+        {
+            TimeSpan timeOut = TimeSpan.FromMilliseconds(timeout);
+
+            var cancellationCompletionSource = new TaskCompletionSource<bool>();
+            try
+            {
+                using (var cts = new CancellationTokenSource(timeOut))
+                {
+                    var task = socket.ConnectAsync(endpoint);
+
+                    using (cts.Token.Register(() => cancellationCompletionSource.TrySetResult(true)))
+                    {
+                        if (task != await Task.WhenAny(task, cancellationCompletionSource.Task))
+                        {
+                            throw new OperationCanceledException(cts.Token);
+                        }
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                throw new SocketException(ConnectionTimedOutStatusCode);
+            }
+        }
+    }
+}

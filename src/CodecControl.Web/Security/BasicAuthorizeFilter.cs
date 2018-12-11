87b0aa17c032b188b9769751893095b00717a5e1
Anders Stenberg <anders.stenberg@activesolution.se>
2018-12-11 13:07:15 +0100

    Upgraded dotnet to 2.1.6

--- src/CodecControl.Web/Security/BasicAuthorizeFilter.cs
+++ src/CodecControl.Web/Security/BasicAuthorizeFilter.cs
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 // -------------------------------------------------------------------------------------------------
 //
-// Kod från:
+// Code from:
 // https://www.johanbostrom.se/blog/adding-basic-auth-to-your-mvc-application-in-dotnet-core
 //
 namespace CodecControl.Web.Security

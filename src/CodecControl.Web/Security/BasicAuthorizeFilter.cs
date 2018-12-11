85bd30c5728e6b8d3b34778d5b35cdff5bcf1397
Anders Stenberg <anders.stenberg@activesolution.se>
2018-12-11 12:39:51 +0100

    added license header

--- src/CodecControl.Web/Security/BasicAuthorizeFilter.cs
+++ src/CodecControl.Web/Security/BasicAuthorizeFilter.cs
@@ -1,11 +1,31 @@
-﻿// -------------------------------------------------------------------------------------------------
-// Copyright (c) Johan Boström. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-// -------------------------------------------------------------------------------------------------
-//
-// Kod från:
-// https://www.johanbostrom.se/blog/adding-basic-auth-to-your-mvc-application-in-dotnet-core
-//
+﻿#region copyright
+/*
+ * Copyright (c) 2018 Sveriges Radio AB, Stockholm, Sweden
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ #endregion
+
 namespace CodecControl.Web.Security
 {
     using System;
@@ -38,9 +58,7 @@ namespace CodecControl.Web.Security
                 }
             }
 
-            // Return authentication type (causes browser to show login dialog)
             context.HttpContext.Response.Headers["WWW-Authenticate"] = "Basic";
-
             context.Result = new UnauthorizedResult();
         }
 
